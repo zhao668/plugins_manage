5b0fff2459f7342005c18e1f7adb603ec995bdf9
zhao <jnzhao2021@163.com>
2022-06-30 12:20:23 +0800

    七牛云上传图片

--- src/main/java/com/ym/pojo/Plugins.java
+++ src/main/java/com/ym/pojo/Plugins.java
@@ -40,4 +40,10 @@ public class Plugins {
 
     @ApiModelProperty(value = "是否审核的标志")
     private Integer flag;
+
+    @ApiModelProperty(value = "作者邮箱")
+    private String email;
+
+    @ApiModelProperty(value = "插件方法名")
+    private String method;
 }
